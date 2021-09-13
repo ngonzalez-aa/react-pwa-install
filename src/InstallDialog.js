939494bcef8ceba9cd59bcabf0b8696b64493e55
ngonzalez-aa <90633394+ngonzalez-aa@users.noreply.github.com>
2021-09-13 12:09:50 -0300

    cambio titulos que estaban en ingles a español

--- src/InstallDialog.js
+++ src/InstallDialog.js
@@ -15,7 +15,7 @@ export default function InstallDialog(props) {
           )}
           {!!props.features && (
             <Box>
-              <Typography variant="subtitle1">Key Features:</Typography>
+              <Typography variant="subtitle1">Características:</Typography>
               <Typography variant="body2" component="div">
                 {props.features}
               </Typography>
@@ -24,7 +24,7 @@ export default function InstallDialog(props) {
         </Box>
         {!!props.description && (
           <>
-            <Typography variant="subtitle1">Description:</Typography>
+            <Typography variant="subtitle1">Descripción:</Typography>
             <Typography variant="body2" component="div">
               {props.description}
             </Typography>
